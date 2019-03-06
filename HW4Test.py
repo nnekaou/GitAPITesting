62c2ed25abc5ccb24a2c3757a0def138bc1dcf74
nnekaou <42782069+nnekaou@users.noreply.github.com>
2019-03-05 23:02:50 -0500

    Update HW4Test.py

--- HW4Test.py
+++ HW4Test.py
@@ -1,20 +1,4 @@
 import unittest
-from unittest import mock
-from unittest.mock import MagicMock as Mock
-from HW4 import GitZeHub
-from unittest.mock import patch
-m = Mock()
-
-class TestGitHubMagic(unittest.TestCase):
-    @patch('requests.get')
-    def JSONTest(self, json):
-        json_object = json.loads(json)
-        m.json.return_value = json_obj
-        json_results_list = []
-        results = [Mock(), Mock(), Mock()]
-        json_results_list.append(json.loads('[{"name" : "Classify-Triangles"}. {"name": "Hello-World"}, {"name": "HW2a567"}, {"name": "HW4"}]'))
-        json_results_list.append(json.loads('[ { "commit" : "1" }, { "commit" : "10" }, { "commit" : "2" }, { "commit" : "29" },  { "commit" : "18" } ]'))
-        m.json.side_effect = results
 
 if __name__ == '__main__':
     print('Testing...')
