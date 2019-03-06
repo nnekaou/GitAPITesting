55a983e0a4a06651a0e13e8ff4e6979ea95b310c
nnekaou <42782069+nnekaou@users.noreply.github.com>
2019-03-05 23:07:11 -0500

    Update HW4Test.py

--- HW4Test.py
+++ HW4Test.py
@@ -1,4 +1,12 @@
 import unittest
+from HW4 import GitZeHu
+
+class testHW4GitHub(unittest.TestCase):
+    def testUser1(self):
+        self.assertEqual(gitHubAPI("nudeagba"), [ ('GitAPITesting', 1), ('TriTesting', 5), ('GEDcom', 1), ('starterweb', 3)])  
+    
+    def testUser2(self):
+        self.assertEqual(gitHubAPI("richkempinski"), [('hellogitworld', 30), ('helloworld', 2), ('Project1', 2), ('threads-of-life', 1)])
 
 if __name__ == '__main__':
     print('Testing...')
